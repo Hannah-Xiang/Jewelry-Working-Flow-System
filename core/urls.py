484ebcf6f03fc607262faf2cffcfd552bf5e9e5f
hannah <yxiang2002@gmail.com>
2026-06-11 15:38:48 -0400

    html initial

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('new-ticket/', views.new_ticket, name='new_ticket'),
+    path('tickets/', views.all_tickets, name='all_tickets'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('customers/', views.customers, name='customers'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'core/dashboard.html')
+
+def new_ticket(request):
+    return render(request, 'core/new_ticket.html')
+
+def all_tickets(request):
+    return render(request, 'core/all_tickets.html')
+
+def calendar(request):
+    return render(request, 'core/calendar.html')
+
+def customers(request):
+    return render(request, 'core/customers.html')
+
+def customers(request):
+    return render(request, 'core/base.html')
